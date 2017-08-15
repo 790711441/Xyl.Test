b5d98982479c41ba60b73aea339d93098a30e683
Administrator <Administrator@PC-20160803THXZ>
2017-08-15 11:15:06 +0800

    添加项目文件。

---
+++ Xyl.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Xyl.Test.MvcApplication" Language="C#" %>
---
+++ Xyl.Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xyl.Test.Startup))]
+namespace Xyl.Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
